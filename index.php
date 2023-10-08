98b161bac95c90303ba352ccfee7a544dfee40eb
Oghenetega Gbiyede <tega2112@gmail.com>
2023-10-08 15:05:57 +0100

    Update index.php

--- index.php
+++ index.php
@@ -9,7 +9,10 @@
   <link rel="shortcut icon" type="image/png" href="img/icon.png" />
   <link rel="icon" href="img/logo.png" />
   <link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600&display=swap" rel="stylesheet" />
-  <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous"> -->
+  <!-- <link href="ht
+  
+  
+  tps://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous"> -->
 
   <link rel="stylesheet" href="style.css" />
   <title>Bank App for koren</title>
